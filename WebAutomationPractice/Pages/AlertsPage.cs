5efb8410b7113454e1f4c8160399d2a7e855a48b
m.zielonka <email@we.ew>
2022-05-13 00:36:02 +0200

    Finished Alerts PageObject

--- WebAutomationPractice/Pages/AlertsPage.cs
+++ WebAutomationPractice/Pages/AlertsPage.cs
@@ -34,6 +34,60 @@ namespace WebAutomationPractice.Pages
             ClickButton("/html/body/main/div/button[4]");
         }
 
+        public void AcceptAlert()
+        {
+            var alert = driver.SwitchTo().Alert();
+            alert.Accept();
+        }
+
+        public void DismissAlert()
+        {
+            var alert = driver.SwitchTo().Alert();
+            alert.Dismiss();
+        }
+
+        public string GetPromptAlertText()
+        {
+            var alert = driver.SwitchTo().Alert();
+            return alert.Text;
+        }
+
+        public void SetPromptAlertText(string text)
+        {
+            var alert = driver.SwitchTo().Alert();
+            alert.SendKeys(text);
+        }
+
+        public string GetSimpleAlertLabel()
+        {
+            var text = GetLabelText("/html/body/main/div/p[1]");
+            return text;
+        }
+
+        public string GetPropmptAlertLabel()
+        {
+            var text = GetLabelText("/html/body/main/div/p[2]");
+            return text;
+        }
+
+        public string GetConfirmAlertLabel()
+        {
+            var text = GetLabelText("/html/body/main/div/p[3]");
+            return text;
+        }
+
+        public string GetDelayedAlertLabel()
+        {
+            var text = GetLabelText("/html/body/main/div/p[4]");
+            return text;
+        }
+
+        private string GetLabelText(string xPath)
+        {
+            var label = driver.FindElement(By.XPath(xPath));
+            return label.Text;
+        }
+
         private void ClickButton(string xPath)
         {
             var button = driver.FindElement(By.XPath(xPath));
