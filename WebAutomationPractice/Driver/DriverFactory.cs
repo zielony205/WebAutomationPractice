c72cdba73212b9115df973843c994c3abcae868e
m.zielonka <email@we.ew>
2022-05-13 00:56:39 +0200

    Code cleanup

--- WebAutomationPractice/Driver/DriverFactory.cs
+++ WebAutomationPractice/Driver/DriverFactory.cs
@@ -1,11 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebAutomationPractice.Driver
 {
